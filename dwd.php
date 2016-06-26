f3c1229b4c5b31def411890bad0b67445958b99b
Niklas <nkreer@users.noreply.github.com>
2016-06-26 14:17:29 +0200

    Better searching

--- dwd.php
+++ dwd.php
@@ -10,23 +10,23 @@ $data = json_decode($data, true);
 if(empty($query)){
 	echo "Aktuell ".count($data["warnings"])." Wetterwarnungen";
 } else {
-	$warn = ["percentage" => 0, "place" => "Nowhere", "warning" => "Keine Wetterwarnung", "instruction" => "Bleiben Sie cool!"];
+	$warn = [];
 	foreach($data["warnings"] as $warning){
-		similar_text($warning[0]["regionName"], $query, $percentage);
-		if($percentage > $warn["percentage"]){
+		if(stripos($warning[0]["regionName"], $query) !== false){
 			$warn["place"] = $warning[0]["regionName"];
 			$warn["warning"] = $warning[0]["headline"];
 			$warn["description"] = $warning[0]["description"];
 			$warn["start"] = $warning[0]["start"] / 1000;
 			$warn["end"] = $warning[0]["end"] / 1000;#
-			$warn["percentage"] = $percentage;
-		}
-		if($percentage === 100){
-			break; //Cancel loop, we have an exact result
+			break;
 		}
 	}
-	echo $warn["place"]." :: ";
-	echo $warn["warning"]." :: ";
-	echo date("d.m.y H:i:s", $warn["start"]).($warn["end"] ? " - ".date("d.m.y H:i:s", $warn["end"]) : "");
-	echo " :: ".$warn["description"];
+	if(!empty($warn)){
+		echo $warn["place"]." :: ";
+		echo $warn["warning"]." :: ";
+		echo date("d.m.y H:i:s", $warn["start"]).($warn["end"] ? " - ".date("d.m.y H:i:s", $warn["end"]) : "");
+		echo " :: ".$warn["description"];
+	} else {
+		echo "Aktuell liegen keine Wetterwarnungen für diesen Ort vor.";
+	}
 }
