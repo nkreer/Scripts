aa121ba68728fda177f5321c735775990cf72169
Niklas <nkreer@users.noreply.github.com>
2016-07-09 11:25:26 +0200

    Update itunes.php

--- Media/itunes.php
+++ Media/itunes.php
@@ -1,16 +1,26 @@
 <?php
 
+include_once("src/IRC/Utils/ArgumentParser.php");
+use IRC\Utils\ArgumentParser;
+
 for($i = 0; $i <= 2; $i++) unset($argv[$i]);
-$query = urlencode(implode(" ", $argv));
+$info = ArgumentParser::parse(implode(" ", $argv), ["country"]);
+$query = urlencode($info["text"]);
+if(!empty($info["country"])){
+    $country = urlencode($info["country"]);
+} else {
+    $country = "US";
+}
 
 if($query){
-    $data = json_decode(file_get_contents("https://itunes.apple.com/search?term=".$query), true);
+    $data = json_decode(file_get_contents("https://itunes.apple.com/search?limit=1&country=".$country."&term=".$query), true);
     if(!empty($data["results"][0])){
         $result = $data["results"][0];
         echo $result["kind"]." :: ";
         echo $result["artistName"]." :: ";
         echo $result["trackName"]." :: ";
         echo ($result["trackPrice"] > 0 ? $result["trackPrice"]." ".$result["currency"] : "Free")." :: ";
+        echo $result["country"]." :: ";
         echo $result["primaryGenreName"];
         if(!empty($result["shortDescription"])){
             echo " :: ".$result["shortDescription"];
--- README.md
+++ README.md
@@ -30,7 +30,6 @@ This repository contains all scripts I personally use as commands with my [Scrip
 | define.php | Get urban dictionary definitions | `define <query>` |
 | fact.php | Random fact from numbersapi.com | `fact [number]` |
 | github.php | Search GitHub repositories | `github <query>` |
-| itunes.php | Search the iTunes Store | `itunes <query>` |
 | location.php | Get latitude and longitude of a given address | `location <address>` |
 | packagist.php | Search packagist | `packagist <query>`|
 | safe.php | Check is a website is suspicous | `safe <url>` |
@@ -43,6 +42,7 @@ This repository contains all scripts I personally use as commands with my [Scrip
 | Script | Description | Fish-Usage |
 |--------|-------------|------------|
 | book.php | Get information about a book from Google | `book <query>` |
+| itunes.php | Search the iTunes Store | `itunes <query> [-c country]` |
 | omdb.php | Search the Open Movie Database | `omdb <query>` |
 | spotify.php | Get song previews | `spotify <query>` |
 | topic.php | Get a random hot topic | `topic` |
