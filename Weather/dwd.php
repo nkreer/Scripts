d3ecc1ea004de53b9cd97f5593772fa407d567f1
Niklas <nkreer@users.noreply.github.com>
2016-07-22 15:35:42 +0200

    Multiple DWD Warnings

--- Weather/dwd.php
+++ Weather/dwd.php
@@ -1,40 +1,49 @@
 <?php
 
 //This script is made for people living in germany, so string are in Deutsch.
+include_once("src/IRC/Utils/ArgumentParser.php");
+use IRC\Utils\ArgumentParser;
 
 for($i = 0; $i <= 2; $i++) unset($argv[$i]);
 
 $check = ["warnings", "vorabInformation"];
 $query = implode(" ", $argv);
+$parse = ArgumentParser::parse($query, ["warning"]);
 
-$data = str_ireplace(["warnWetter.loadWarnings(", ");"], ["", ""], file_get_contents("http://www.dwd.de//DWD/warnungen/warnapp/json/warnings.json"));
+$data = str_ireplace(["warnWetter.loadWarnings(", ");"], ["", ""], file_get_contents("http://www.dwd.de//DWD/warnungen/warnapp_landkreise/json/warnings.json"));
 $data = json_decode($data, true);
 
-if(empty($query)){
+if(empty($parse["text"])){
     echo "Aktuell ".count($data["warnings"])." amtliche Wetterwarnungen und ".count($data["vorabInformation"])." Vorabinformationen";
 } else {
+    if(empty($parse["warning"])){
+        $wn = 0;
+    } else {
+        $wn = (int)$parse["warning"] - 1;
+    }
     foreach($check as $info){
         $warn = [];
         foreach($data[$info] as $warning){
-            if(stripos($warning[0]["regionName"], $query) !== false){
-                $warn["place"] = $warning[0]["regionName"];
-                $warn["state"] = $warning[0]["stateShort"];
-                $warn["warning"] = $warning[0]["event"];
-                $warn["description"] = $warning[0]["description"];
-                $warn["start"] = $warning[0]["start"] / 1000;
-                $warn["end"] = $warning[0]["end"] / 1000;#
+            if(stripos($warning[$wn]["regionName"], $parse["text"]) !== false){
+                $warn["warnings"] = count($warning);
+                $warn["place"] = $warning[$wn]["regionName"];
+                $warn["state"] = $warning[$wn]["stateShort"];
+                $warn["warning"] = $warning[$wn]["event"];
+                $warn["description"] = $warning[$wn]["description"];
+                $warn["start"] = $warning[$wn]["start"] / 1000;
+                $warn["end"] = $warning[$wn]["end"] / 1000;
                 break;
             }
         }
         if(!empty($warn)){
+            echo ($wn + 1)."/".$warn["warnings"]." Warnungen :: ";
             echo $warn["place"].", ".$warn["state"]." :: ";
             echo $warn["warning"]." :: ";
-            echo date("d.m.y H:i:s", $warn["start"]).($warn["end"] ? " - ".date("d.m.y H:i:s", $warn["end"]) : "");
+            echo date("d.m.y H:i:s e", $warn["start"]).($warn["end"] ? " - ".date("d.m.y H:i:s e", $warn["end"]) : "");
             echo " :: ".$warn["description"];
             break;
         } elseif($info === $check[1]){
-            echo "Aktuell liegen keine Wetterwarnungen oder Vorabinformationen für diesen Ort vor.";
+            echo "Aktuell liegt keine Wetterwarnung oder Vorabinformation für diesen Ort vor.";
         }
     }
-    
 }
