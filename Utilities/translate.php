dd54962d4cb0b02944dee094eec4287afb8a4f60
Niklas <nkreer@users.noreply.github.com>
2016-12-30 10:33:33 +0100

    Fix little problems with the translation

--- Utilities/translate.php
+++ Utilities/translate.php
@@ -3,10 +3,10 @@
 for($i = 0; $i <= 2; $i++) unset($argv[$i]);
 $query = urlencode(implode(" ", $argv));
 
-$data = @file_get_contents("http://www.transltr.org/api/translate?text=".$query."&to=en)");
+$data = @file_get_contents("http://www.transltr.org/api/translate?text=".$query."&to=en");
 if($data){
 	$data = json_decode($data, true);
-	if(isset($data["translationText"]) and strlen($data["translationText"])){
+	if(isset($data["translationText"]) and strlen($data["translationText"]) <= 300){
 		echo "[".strtoupper($data["from"])."-".strtoupper($data["to"])."] ".$data["translationText"];
 	} else {
 		die("Error :: Translation too long.");
